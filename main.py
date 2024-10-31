dfdc7c1a1df2f41c40f133a6b2751a2d61241ff4
Creation Duwal <creationduwal@gmail.com>
2024-10-31 21:10:19 +0545

    chore: allow commands to be run only in servers

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
-from discord.ext import tasks, commands
+from discord.ext import commands, tasks
 import requests
 from datetime import datetime
 import random
 from dotenv import load_dotenv
 import os
+from table2ascii import table2ascii as t2a, PresetStyle
 from keep_alive import keep_alive
 
 # keep_alive()
@@ -15,41 +16,64 @@ intents.message_content = True  # Enable the intent for message content
 # Create the bot with the specified intents
 bot = commands.Bot(intents=intents, command_prefix="!")
 
+# the way 'servers' variable is used:
+
+# {
+#     guild_id: {
+#         notifications_channel: channel_id,
+#         game_number: game_number,
+#         user_id[]: {
+#             distance: distance,
+#             hints: hints_used,
+#             guesses_made: guesses_made,
+#         },
+#     },
+
 servers = {}
 
 # Constants
-GAME_NUMBER = random.randint(1, 300)  # Initial game number
+GAME_NUMBER = (
+    770 + (datetime.now() - datetime(2024, 10, 30)).days
+)  # Initial game number
 MAX_HINTS = 3  # Maximum number of hints per day
-hint_usage = {}  # Track hint usage by user
-guesses_made = 0  # Track total guesses made
-yesterdays_word = ""  # Store yesterday's word
-distance = None
-start_date = None
 
 
 @bot.event
 async def on_guild_join(guild):
-    servers[guild.id] = {"notifications_channel": None}
+    servers[guild.id] = {"notifications_channel": None, "game_number": GAME_NUMBER}
 
 
 # Event when the bot is ready
 @bot.event
 async def on_ready():
-    global start_date, servers
-    start_date = datetime.now().date()  # Initialize the start date
+    global servers
     print(f"Bot {bot.user.name} is ready!")
     for guild in bot.guilds:
-        servers[guild.id] = {"notifications_channel": None}
+        servers[guild.id] = {"notifications_channel": None, "game_number": GAME_NUMBER}
 
     await daily_game_logic()
 
 
 @bot.slash_command(name="set_channel", description="Set the notifications channel")
 async def set_channel(ctx: discord.ApplicationContext, channel: discord.TextChannel):
+    ## check if comamnd is being called from a guild
+    if not ctx.guild:
+        await ctx.interaction.response.send_message(
+            content=f"This command can only be used in a server", ephemeral=True
+        )
+        return
+
+    if not (ctx.author.guild_permissions.manage_channels):
+        await ctx.interaction.response.send_message(
+            content=f"You do not have access to this command", ephemeral=True
+        )
+        return
+
     global servers
     if ctx.guild.id not in servers:
         servers[ctx.guild.id] = {
             "notifications_channel": channel.id,
+            "game_number": GAME_NUMBER,
         }
     else:
         servers[ctx.guild.id]["notifications_channel"] = channel.id
@@ -66,11 +90,20 @@ async def guess(
     ctx: discord.ApplicationContext,
     word: str,
 ):
-    global guesses_made, distance, GAME_NUMBER, hint, servers
-    if ctx.guild_id not in servers:
-        servers[ctx.guild_id] = {"notifications_channel": None}
+    if not ctx.guild:
+        await ctx.interaction.response.send_message(
+            content=f"This command can only be used in a server", ephemeral=True
+        )
+        return
 
-    api_url = f"https://api.contexto.me/machado/en/game/{GAME_NUMBER}/{word}"
+    global distance, hint, servers
+    if ctx.guild_id not in servers:
+        servers[ctx.guild_id] = {
+            "notifications_channel": None,
+            "game_number": GAME_NUMBER,
+        }
+
+    api_url = f"https://api.contexto.me/machado/en/game/{servers[ctx.guild_id]['game_number']}/{word}"
 
     # Fetch data from the Contexto API
     try:
@@ -84,18 +117,52 @@ async def guess(
                     if ctx.author.id in servers[ctx.guild_id]
                     else 1
                 ),
+                "hints": 0,
             }
             await ctx.respond(
                 f"You guessed: **{word}**\nDistance: {data['distance']}\n"
             )
+            distance = data["distance"]
             if distance == 0 or distance == 1:
                 await ctx.send(f"You won")
-                GAME_NUMBER = random.randint(1, 300)
+
+                ## send a proper stats report of the game
+                tablestring = t2a(
+                    header=["User", "Distance", "Hints", "Guesses Made"],
+                    body=[
+                        [
+                            bot.get_user(user_id).name,
+                            servers[ctx.guild_id][user_id]["distance"],
+                            servers[ctx.guild_id][user_id]["hints"],
+                            servers[ctx.guild_id][user_id]["guesses_made"],
+                        ]
+                        for user_id in filter(
+                            lambda x: x != "notifications_channel"
+                            and x != "game_number",
+                            servers[ctx.guild_id],
+                        )
+                    ],
+                    style=PresetStyle.thin_compact,
+                )
+                embed = discord.Embed(
+                    title="Stats of the game",
+                    description=tablestring,
+                    color=discord.Color.green(),
+                )
+                await ctx.send(embed=embed)
+
+                ## reset the distance value for all players in this guild
+                for user_id in servers[ctx.guild_id]:
+                    if user_id != "notifications_channel" and user_id != "game_number":
+                        servers[ctx.guild_id][user_id]["distance"] = None
+                        servers[ctx.guild_id][user_id]["hints"] = 0
+                        servers[ctx.guild_id][user_id]["guesses_made"] = 0
+
+                servers[ctx.guild_id]["game_number"] = random.randint(1, 300)
                 hint = 0
                 await ctx.send(
                     f"New word has been chosen. Hints are now available as well"
                 )
-            # Send the response to the user
         else:
             await ctx.respond(
                 "Either u spelled it wrong or used foul word. very bad manners"
@@ -107,9 +174,16 @@ async def guess(
 # Command to request a hint
 @bot.slash_command(name="hint", description="Get a hint to help you guess the word")
 async def usehint(ctx: discord.ApplicationContext):
-    global hint_usage, servers
+    if not ctx.guild:
+        await ctx.interaction.response.send_message(
+            content=f"This command can only be used in a server", ephemeral=True
+        )
+        return
+
+    global servers
     user_id = ctx.author.id
     guild_id = ctx.guild_id
+    guild_game_number = servers[guild_id]["game_number"]
 
     # Initialize hint usage for the user if not already done
     if user_id not in servers[guild_id]:
@@ -158,7 +232,7 @@ async def usehint(ctx: discord.ApplicationContext):
 
     # Fetches hint from the API
     hint_api_url = (
-        f"https://api.contexto.me/machado/en/tip/{GAME_NUMBER}/{hint_distance}"
+        f"https://api.contexto.me/machado/en/tip/{guild_game_number}/{hint_distance}"
     )
 
     try:
@@ -180,17 +254,26 @@ async def usehint(ctx: discord.ApplicationContext):
 
 # Function to handle daily game logic
 async def daily_game_logic():
-    global GAME_NUMBER, guesses_made, hint_usage, yesterdays_word, distance, servers
+    global GAME_NUMBER, distance, servers
 
     ## The game number of 30th October 2024 is 770
     ## The game number increases by 1 each day
     GAME_NUMBER = 770 + (datetime.now() - datetime(2024, 10, 30)).days
-    hint_usage.clear()  # Resets hint usage for all users
     distance = 0
 
     # Informs users about the new game
     for guild_id in servers:
+        ## change the game number for each server
+        servers[guild_id]["game_number"] = GAME_NUMBER
         channel = servers[guild_id]["notifications_channel"]
+
+        ## reset the hints for each user in the guild
+        for user_id in servers[guild_id]:
+            if user_id != "notifications_channel" and user_id != "game_number":
+                servers[guild_id][user_id]["hints"] = 0
+                servers[guild_id][user_id]["distance"] = None
+                servers[guild_id][user_id]["guesses_made"] = 0
+
         if channel is not None:
             await bot.get_channel(channel).send(
                 f"""
@@ -230,16 +313,42 @@ async def daily_game():
 
 @bot.slash_command(name="god", description="Admin Command | Reveal the word")
 async def god(ctx: discord.ApplicationContext):
+    if not (ctx.guild):
+        await ctx.interaction.response.send_message(
+            content=f"This command can only be used in a server", ephemeral=True
+        )
+        return
+
     if not (ctx.author.guild_permissions.manage_channels):
         await ctx.interaction.response.send_message(
             content=f"You do not have access to this command", ephemeral=True
         )
         return
 
-    answer = requests.get(f"https://api.contexto.me/machado/en/giveup/{GAME_NUMBER}")
+    answer = requests.get(
+        f"https://api.contexto.me/machado/en/giveup/{servers[ctx.guild_id]['game_number']}"
+    )
     text = answer.json()["word"]
 
     await ctx.interaction.response.send_message(f"The word was {text}", ephemeral=True)
+
+
+@bot.slash_command(name="reset", description="Admin Command | Reset the game")
+async def reset(ctx: discord.ApplicationContext):
+    if not (ctx.guild):
+        await ctx.interaction.response.send_message(
+            content=f"This command can only be used in a server", ephemeral=True
+        )
+        return
+
+    if not (ctx.author.guild_permissions.manage_channels):
+        await ctx.interaction.response.send_message(
+            content=f"You do not have access to this command", ephemeral=True
+        )
+        return
+
+    await ctx.interaction.response.send_message(f"Game has been reset")
+    await daily_game_logic()
 
 
 daily_game.start()
